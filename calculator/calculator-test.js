03d7f3424e2e0e467aceb22718e1b2570efe1de6
Erick Ricardo Hjort Colunga <ErickHjort@MacBuzz-Pro.local>
2022-07-11 14:09:01 -0500

    Save 11 jul

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,37 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 123456,
+    years: 7,
+    rate: 9
+  }
+  expect(calculateMonthlyPayments(values)).toEqual(1986.29);
 });
 
+it('should calculate the monthly rate correctly', function () {
+  const values = {
+    amount: 96096,
+    years : 3,
+    rate: 7.55
+  }
+  expect(calculateMonthlyPayments(values)).toEqual(2991.39);
+});
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount : 66676,
+    years : 69,
+    rate : 9
+  }
+  expect(calculateMonthlyPayments(values)).toEqual(501.10); 
 });
 
+it("should return a result with 2 decimal places", function() {
+  const values = {
+    amount : 30001,
+    years : 7,
+    rate : 5.8
+    }
+  expect(calculateMonthlyPayments(values)).toEqual(435.40);
+});
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,13 @@
+
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
-    setupIntialValues();
+    setupIntialValues();    
     form.addEventListener("submit", function(e) {
       e.preventDefault();
-      update();
+      if (e.target.tagName === "FORM"){
+        update();       
+        }
     });
   }
 });
@@ -21,20 +24,46 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById("loan-amount").defaultValue = "10000";
+  document.getElementById("loan-years").defaultValue = "5";
+  document.getElementById("loan-rate").defaultValue = "7";
 }
+
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  calculateMonthlyPayment(getCurrentUIValues());
+  updateMonthly();
 }
+
+function zeroDecimal (num) {
+  let decimal = num.split(".");
+  if (decimal[1].length === 1){
+    decimal[1] = decimal[1] + "0";
+    }
+  return decimal[0]+ "." + decimal[1];
+}
+
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+    let i = (values.rate/100) / 12;
+    let dividend =  (values.amount * i);
+    let n = values.years * 12;
+    let divisor = (1 - Math.pow((1 + i), -n));
+    let monthlyPayments = Math.round((dividend/divisor)*100) / 100;
+    monthlyPayments = monthlyPayments.toString();
+    monthlyPayments = zeroDecimal(monthlyPayments);
+    return monthlyPayments;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
-function updateMonthly(monthly) {
+function updateMonthly() {
+  const  monthlyInput = document.getElementById("monthly-payment");
+  monthlyInput.innerText = "$" + calculateMonthlyPayment(getCurrentUIValues());
 }
+
